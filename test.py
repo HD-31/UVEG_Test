54e846922f00facdf9167a14a07440df85b50640
HDC <hd_cem0@gmail.com>
2026-05-03 22:04:27 -0600

    ini

---
+++ test.py
@@ -0,0 +1,18 @@
+def main_menu():
+    while True:
+        print("\nMiniCafe Menu")
+        print("1. Sistema de ventas")
+        print("2. Reporte")
+        print("3. Salir")
+        
+        choice = input('Elegir opción:')
+        if choice == '1':
+            print(" Bienvenido al sistema de ventas")
+            break
+        elif choice == '2':
+            print(" Bienvenido al sistema de reporte")
+            break
+        elif choice == '3':
+            print(" Bye")
+            break
+main_menu()
