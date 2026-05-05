bd37ae320cf316aff0f1564a33d03835adc789d7
HDC <hd_cem0@gmail.com>
2026-05-04 20:17:55 -0600

    fixed title

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def main_menu():
     while True:
-        print("\nMiniCafe Menu")
+        print("\n MiniCafe Menu")
         print("1. Sistema de ventas")
         print("2. Reporte")
         print("3. Salir")
