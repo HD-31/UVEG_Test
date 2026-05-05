bdb9384ae99e17531bac909f0b56e332c8cb5bb7
HDC <hd_cem0@gmail.com>
2026-05-04 19:44:57 -0600

    3

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def main_menu():
         print("\nMiniCafe Menu")
         print("1. Sistema de ventas")
         print("2. Reporte")
-        print("3. Salir2")
+        print("3. Salir3")
         
         choice = input('Elegir opción:')
         if choice == '1':
