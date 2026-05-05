177f2768d789e311a338105935c1c37d30a861c0
HDC <hd_cem0@gmail.com>
2026-05-04 20:45:36 -0600

    super

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def main_menu():
     while True:
-        print("\n MiniCafe Menu")
+        print("\n Supercafe Menu")
         print("1. Sistema de ventas")
         print("2. Reporte")
         print("3. Salir3")
@@ -15,4 +15,4 @@ def main_menu():
         elif choice == '3':
             print(" Bye")
             break
-main_menu()
+main_menu() 
