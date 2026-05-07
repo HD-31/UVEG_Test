b5b89a930bd0d529060171db1e54116a4bc8368f
HDC <hd_cem0@gmail.com>
2026-05-06 21:50:52 -0600

    fixed title

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def main_menu():
     while True:
-        print("\n Supercafe Menu")
+        print("\nSupercafe Menu")
         print("1. Sistema de ventas")
         print("2. Reporte")
         print("3. Salir")
